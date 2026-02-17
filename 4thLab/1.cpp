0e6c999e9af65fe2dbe5f72f8cc9aea953fa7b24
Swastikkashyap-22 <swastikkashyap2006@gmail.com>
2026-02-17 23:42:24 +0530

    AD&A

--- 4thLab/1.cpp
+++ 4thLab/1.cpp
@@ -7,8 +7,55 @@ using namespace std;
 using namespace std::chrono;
 
 void quickSort(int arr[], int n){
+    if(n <= 1) return;
 
+    int *lstack = new int[n];
+    int *hstack = new int[n];
+    int top = -1;
+
+    int l = 0, h = n - 1;
+
+    while(true){
+        while(l < h){
+            int i = l, j = h;
+            int pivot = arr[(l + h) / 2];
+
+            while(i <= j){
+                while(arr[i] < pivot) i++;
+                while(arr[j] > pivot) j--;
+                if(i <= j){
+                    int t = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = t;
+                    i++; 
+                    j--;
+                }
+            }
+
+            if(j - l < h - i){
+                if(i < h){
+                    lstack[++top] = i;
+                    hstack[top] = h;
+                }
+                h = j;
+            } else {
+                if(l < j){
+                    lstack[++top] = l;
+                    hstack[top] = j;
+                }
+                l = i;
+            }
+        }
+
+        if(top < 0) break;
+        l = lstack[top];
+        h = hstack[top--];
+    }
+
+    delete[] lstack;
+    delete[] hstack;
 }
+
 
 void print(int arr[], int n){
     for(int i = 0 ; i < n ; i++){
--- 4thLab/2.cpp
+++ 4thLab/2.cpp
@@ -1,2 +1,60 @@
 // Binary Sort (list divided into n/3 and 2n/3)
 
+#include<iostream>
+#include<random>
+#include<chrono>
+using namespace std;
+using namespace std::chrono;
+
+int binSearch(int arr[], int n, int target){
+    int low = 0, high = n - 1;
+
+    while(low <= high){
+        int mid1 = low + (high - low) / 3;
+        int mid2 = high - (high - low) / 3;
+
+        if(arr[mid1] == target) return mid1;
+        if(arr[mid2] == target) return mid2;
+
+        if(target < arr[mid1])
+            high = mid1 - 1;
+        else if(target > arr[mid2])
+            low = mid2 + 1;
+        else{
+            low = mid1 + 1;
+            high = mid2 - 1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    random_device rd;
+    mt19937 gen(rd() + steady_clock::now().time_since_epoch().count());
+
+    int n = 10000;
+    int* arr = new int[n];
+
+    uniform_int_distribution<> stepDist(1, 10);
+    int currentVal = 0;
+    for(int i = 0; i < n; i++){
+        currentVal += stepDist(gen);
+        arr[i] = currentVal;
+    }
+
+    uniform_int_distribution<> indexDist(0, n - 1);
+    int randomIndex = indexDist(gen);
+    int target = arr[randomIndex];
+
+    auto start = high_resolution_clock::now();
+    int result = binSearch(arr, n, target);
+    auto stop = high_resolution_clock::now();
+
+    auto duration = duration_cast<nanoseconds>(stop - start);
+
+    cout << "Target: " << target << endl;
+    cout << "Index Found: " << result << endl;
+    cout << "Time Taken: " << duration.count() << " microseconds" << endl;
+
+    return 0;
+}
